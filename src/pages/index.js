f05bd1e5af099601e0dcd624cfdd269590dcac65
Paul Handorff <handorff.paul@gmail.com>
2020-09-19 07:01:41 -0400

    feat: support for pasting a comma-separated list of stop ids

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,61 @@ import { useTable } from "react-table"
 import _ from "lodash"
 
 const StopPicker = ({ allStops, setSelectedStops }) => {
-  const onChange = selected => {
-    if (selected) {
-      setSelectedStops(selected.map(({ value }) => value))
+  const [value, setValue] = useState(null)
+  const [inputValue, setInputValue] = useState("")
+
+  const allStopsById = useMemo(
+    () =>
+      _.fromPairs(
+        allStops.map(stop => {
+          const { value } = stop
+          return [value, stop]
+        })
+      ),
+    [allStops]
+  )
+
+  const onPaste = e => {
+    const pastedText = e.clipboardData.getData("Text")
+    const pastedIds = pastedText.split(",").map(s => s.trim())
+    const pastedValues = pastedIds
+      .map(id => allStopsById[id])
+      .filter(v => v !== undefined)
+    setValue(value => [...(value || []), ...pastedValues])
+  }
+
+  useEffect(() => {
+    if (value) {
+      setSelectedStops(value.map(({ value }) => value))
     } else {
       setSelectedStops([])
     }
+  }, [value, setSelectedStops])
+
+  const onChange = newValue => {
+    setValue(newValue)
+  }
+
+  const onInputChange = newInputValue => {
+    setInputValue(newInputValue)
   }
 
   if (allStops.length > 0) {
     return (
-      <Select
-        isMulti
-        onChange={onChange}
-        name="stops"
-        options={allStops}
-        className="basic-multi-select"
-        classNamePrefix="select"
-        filterOption={createFilter({ ignoreAccents: false })}
-      />
+      <div onPaste={onPaste}>
+        <Select
+          value={value}
+          inputValue={inputValue}
+          onChange={onChange}
+          onInputChange={onInputChange}
+          isMulti
+          name="stops"
+          options={allStops}
+          className="basic-multi-select"
+          classNamePrefix="select"
+          filterOption={createFilter({ ignoreAccents: false })}
+        />
+      </div>
     )
   } else {
     return <div>loading stop data...</div>
